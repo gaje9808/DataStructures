0ff0c4d4f9f7b8ceaacff9b95ba3d549e2b797d6
Gajendra Kumar <gaje9808@gmail.com>
2019-08-20 07:18:31 +0530

    Added code for detection using bfs

--- Graphs/src/main/java/com/self/basics/DetectCycleInUnDirectedGraph.java
+++ Graphs/src/main/java/com/self/basics/DetectCycleInUnDirectedGraph.java
@@ -3,39 +3,80 @@ package com.self.basics;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class DetectCycleInUnDirectedGraph {
-//using DFS we can solve this easily as we want to establish a parent child relationship between two vertices
-//which can't be done very easily with bfs.
-// To asset that there's a cycle we have to come back to the original point of starting after certain number of
-// traversals. In dfs we always have know the recent parent of a vertex and hence finding a cycle becomes really easy.
-    public static boolean isCyclicdfs(Graph g) {
+
+//------------------------------ ******* USING DFS******--------------------------------
+//
+//    public static boolean isCyclicdfs(Graph g) {
+//        int v = g.vertices;
+//        boolean[] visited = new boolean[v];
+//
+//        Arrays.fill(visited,false);
+//        for (int i=0;i<v;i++)
+//            if (!visited[i]) {
+//                if (isCyclicUtildfs(i,visited,g, -1))
+//                    return true;
+//            }
+//        return false;
+//    }
+//
+//    public static boolean isCyclicUtildfs(int i, boolean[] visited, Graph g, int parent) {
+//        visited[i] = true;
+//
+//        Iterator<Integer> it = g.adj[i].listIterator();
+//        while (it.hasNext()) {
+//            int n = it.next();
+//            if (!visited[n]) {
+//                if(isCyclicUtildfs(n,visited,g,i))
+//                    return true;
+//            }
+//            else if (i!= parent) return true;
+//        }
+//        return false;
+//    }
+//--------------------********************--------------------------------------------------
+
+//--------------------**********USING BFS*******--------------------------------------------
+
+    public static boolean isCyclicbfs(Graph g) {
         int v = g.vertices;
         boolean[] visited = new boolean[v];
+        Arrays.fill(visited, false);
 
-        Arrays.fill(visited,false);
         for (int i=0;i<v;i++)
-            if (!visited[i]) {
-                if (isCyclicUtildfs(i,visited,g, -1))
-                    return true;
-            }
+            if (!visited[i] && isCyclicbfsUtil(i, visited, g))
+                return true;
         return false;
     }
 
-    public static boolean isCyclicUtildfs(int i, boolean[] visited, Graph g, int parent) {
+    public static boolean isCyclicbfsUtil(int i, boolean[] visited, Graph g) {
+        int v = g.vertices;
+        Queue<Integer> q = new LinkedList<>();
         visited[i] = true;
+        q.add(i);
 
-        Iterator<Integer> it = g.adj[i].listIterator();
-        while (it.hasNext()) {
-            int n = it.next();
-            if (!visited[n]) {
-                if(isCyclicUtildfs(n,visited,g,i))
+        int[] parent = new int[v];
+        Arrays.fill(parent, -1);
+
+        while (!q.isEmpty()) {
+            i  = q.poll();
+            Iterator<Integer> it = g.adj[i].listIterator();
+            while (it.hasNext()){
+                int n = it.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    q.add(n);
+                    parent[n] = i;
+                }
+                else if(parent[n] != i)
                     return true;
             }
-            else if (i!= parent) return true;
         }
         return false;
     }
+//-----------------------******************---------------------------------------------
 
     // driver method similar to the one on geeks for geeks
     public static void main(String args[]) {
@@ -45,7 +86,8 @@ public class DetectCycleInUnDirectedGraph {
         g1.addEdge(2, 0);
         g1.addEdge(0, 3);
         g1.addEdge(3, 4);
-        if (isCyclicdfs(g1))
+//        if (isCyclicdfs(g1))
+        if (isCyclicbfs(g1))
             System.out.println("Graph contains cycle");
         else
             System.out.println("Graph doesn't contains cycle");
@@ -53,7 +95,8 @@ public class DetectCycleInUnDirectedGraph {
         Graph g2 = new Graph(3);
         g2.addEdge(0, 1);
         g2.addEdge(1, 2);
-        if (isCyclicdfs(g2))
+//        if (isCyclicdfs(g2))
+        if (isCyclicbfs(g2))
             System.out.println("Graph contains cycle");
         else
             System.out.println("Graph doesn't contains cycle");
